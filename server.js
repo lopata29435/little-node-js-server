7f070a9cd7639a2bb6cdf32878d9bba3bd36ed18
lopata29435 <147801802+lopata29435@users.noreply.github.com>
2023-12-27 18:20:04 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Добавьте middleware для обработки CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // * разрешает доступ со всех доменов
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Привет от сервера' });
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
